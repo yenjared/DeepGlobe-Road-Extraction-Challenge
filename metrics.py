7085af3a02e9552eb864d758040b6d308721ed45
yenjared <yenjared15@gmail.com>
2022-08-13 06:25:28 +0000

    Changed test.py to work on cpu... ~1min/image

--- metrics.py
+++ metrics.py
@@ -1,4 +1,4 @@
 import numpy as np
 
 def seg_iou(mask,gt):
-  return np.sum(np.logical_and(mask,gt))/np.sum(np.logical_or(mask,gt))
+  return np.sum(np.logical_and(mask>128,gt>128)) / np.sum(np.logical_or(mask>128,gt>128))
